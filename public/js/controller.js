7b9fad161744dbe8636aff154db4aabcafed23d0
anomitroid <anomitroid2005@gmail.com>
2024-07-18 01:16:24 +0530

    added mobile adaptation

--- public/js/controller.js
+++ public/js/controller.js
@@ -4,7 +4,7 @@ class GameController {
         this.model = model;
         this.view = view;
         this.socket = socket;
-    
+
         this.setupSocketListeners();
         this.setupInputListeners();
     }
@@ -29,10 +29,18 @@ class GameController {
     }
   
     setupInputListeners() {
-        document.addEventListener('mousemove', (e) => {
-            const canvasRect = this.view.canvas.getBoundingClientRect();
-            const mouseY = e.clientY - canvasRect.top;
-            this.socket.emit('paddleMove', mouseY - 50); // 50 is half the paddle height
-        });
+        this.view.canvas.addEventListener('mousemove', (e) => this.handleInput(e.clientY));
+        this.view.canvas.addEventListener('touchmove', (e) => {
+            e.preventDefault();
+            const touch = e.touches[0];
+            this.handleInput(touch.clientY);
+        }, { passive: false });
+    }
+
+    handleInput(clientY) {
+        const canvasRect = this.view.canvas.getBoundingClientRect();
+        const y = clientY - canvasRect.top;
+        const scaledY = y / this.view.scale;
+        this.socket.emit('paddleMove', scaledY - 50); // 50 is half the paddle height
     }
 }
